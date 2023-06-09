726a56a60ada2be622d92646ccc06e72df5e0304
olaoot <furin1998@gmail.com>
2023-06-09 07:25:51 +0000

    feat(naming): add wandb_naming

---
+++ input/code/utils/naming.py
@@ -0,0 +1,6 @@
+def wandb_naming(wandb_name, model_name, batch_size, max_epoch, learning_rate, warmup_steps, weight_decay):
+    params = f'{model_name} | batch_size : {batch_size} | max_epoch : {max_epoch} | lr : {learning_rate} | warmup_steps: {warmup_steps} | weight_decay: {weight_decay}'
+    if wandb_name:
+        return f'{wandb_name} | {params}'
+    else:
+        return params
